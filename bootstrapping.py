5ee4ec486ea16cc98fd4cf00aabece0442c8d5f8
Taiqi He <8318200+taiqihe@users.noreply.github.com>
2021-07-18 21:01:13 -0700

    Fixed bootstrapping.py on non-Linux systems

--- bootstrapping.py
+++ bootstrapping.py
@@ -1,5 +1,3 @@
-
-# Simple bootstrapping procedure to get a confidence interval
 import sys
 import random
 from multiprocessing import Pool
@@ -11,22 +9,23 @@ def b1sample(placeholder):
 	s = random.choices(ARR, k = len(ARR))
 	return np.mean(s)
 
-def bootstrap(data, trials = 10000):
+def worker_init(arr):
 	global ARR
-	ARR = data
-	with Pool(8) as p:
+	ARR = arr
+
+def bootstrap(data, trials = 10000):
+	with Pool(8, initializer = worker_init, initargs = (data,)) as p:
 		dist = p.map(b1sample, range(trials))
 	return dist
 
 def main():
 	random.seed()
 	data = []
-	# Takes a text file consisting of only numbers to be tested, separated by space or tab
 	with open(sys.argv[1]) as fin:
 		data = fin.read().rstrip().split()
 		data = [float(x) for x in data]
 	dist = bootstrap(data, 100000)
-	print(f'Mean {np.mean(dist)}, 95% CI: {np.percentile(dist, 2.5)}, {np.percentile(dist, 97.5)}')
+	print(f'Mean {np.mean(dist)}, 95% CI: {np.percentile(dist, 2.5)}\t{np.percentile(dist, 97.5)}')
 	
 
 if __name__ == '__main__':
